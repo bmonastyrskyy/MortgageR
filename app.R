401b2d0e1a7e88134277f6e3df41e8b818c5d266
Bohdan Monastyrskyy <bmonast@gmail.com>
2015-10-26 15:08:33 -0700

    added mechanism to switch between "fixed" and "adjusted" types of loans

--- app.R
+++ app.R
@@ -6,10 +6,15 @@ ui <- fluidPage(
   headerPanel("Mortgage Calculator"),
   sidebarLayout(
     sidebarPanel(
+      radioButtons(inputId = 'loanType', label = h3("Type of Loan"),
+                   choices = list("Fixed" = 'fixed', "Adjusted" = 'adjusted'),
+                   selected = 'fixed'),
       numericInput(inputId = "loan", "Loan", value = 250000, step = 5000),
       numericInput(inputId = "period", "Period (years)", value = 30, step = 1),
-      numericInput(inputId = "APR", "APR", value = 4.0, step = 0.05),
-      numericInput(inputId = "payment", "Payment", value = NA)
+      textInput(inputId = "APR", "APR", value = "4.0"),
+      numericInput(inputId = "payment", "Payment", value = NA),
+      textInput(inputId = "breaks", label = "Breaks", value = NA),
+      submitButton("Update")
     ),
     mainPanel(
       tabsetPanel(
@@ -23,11 +28,20 @@ ui <- fluidPage(
 )
 
 server <- function(input, output){
-  re.dfm <- reactive(mortgagePaymentFixed(loan = input$loan, APR = input$APR, period = (input$period * 12),
-                                          payment = ifelse(is.null(input$payment) | is.na(input$payment), NA, input$payment)))
+  re.dfm <- reactive({ switch (input$loanType,
+                               'fixed' =
+                        mortgagePaymentFixed(loan = input$loan, APR = input$APR, period = (input$period * 12),
+                                          payment = ifelse(is.null(input$payment) | is.na(input$payment), NA, input$payment)),
+                                'adjusted' =
+                        mortgagePaymentAdjusted(loan = input$loan, APR = input$APR, period = (input$period * 12), breaks = input$breaks,
+                                                payment = ifelse(is.null(input$payment) | is.na(input$payment), NA, input$payment))
+                        )
+                      })
+
   output$summary <- renderPrint({
-    loan.summary(re.dfm())
+    loan.summary(re.dfm());
   })
+
   output$table <- renderTable(re.dfm())
   output$payment <- renderPlot(plotPayment(re.dfm()))
   output$amort <- renderPlot(plotAmortization(re.dfm()))
